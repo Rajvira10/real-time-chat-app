d2e7de08fbdbd3996ddd8ab0423319f208494db4
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-25 23:24:55 +0600

    added the user image at the bottom fetched from google and github

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { User } from "@prisma/client";
+import Image from "next/image";
+import { FC } from "react";
+
+interface AvatarProps {
+  user?: User;
+}
+
+const Avatar: FC<AvatarProps> = ({ user }) => {
+  return (
+    <div className="relative">
+      <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+        {}
+        <Image
+          alt="Avatar"
+          src={user?.image || "/images/placeholder.jpg"}
+          fill
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Avatar;
