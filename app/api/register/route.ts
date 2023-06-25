3139f2c0846f30be635270877902bc141c088e17
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-25 13:19:11 +0600

    added the signup api

---
+++ app/api/register/route.ts
@@ -0,0 +1,28 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { email, name, password } = body;
+  try {
+    if (!email || !name || !password) {
+      return new NextResponse("Missing fields", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    return new NextResponse(error.message, { status: 500 });
+  }
+}
