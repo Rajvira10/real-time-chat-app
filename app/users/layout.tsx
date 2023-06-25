d9e45f4889cc860918f9ad8aee6e5258b95c31f4
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-25 23:39:01 +0600

    added an all users actions

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,20 @@
 import Sidebar from "../components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 interface layoutProps {
   children: React.ReactNode;
 }
 
 const layout = async ({ children }: layoutProps) => {
+  const users = await getUsers();
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>;
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
+      ;
     </Sidebar>
   );
 };
