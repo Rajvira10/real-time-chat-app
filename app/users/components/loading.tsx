7f841450a8c8398d63864ae476997e70486662eb
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-27 02:14:59 +0600

    added spinner in all loading statses

---
+++ app/users/components/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingModal from "@/app/components/LoadingModal";
+import { FC } from "react";
+
+interface loadingProps {}
+
+const loading: FC<loadingProps> = ({}) => {
+  return <LoadingModal />;
+};
+
+export default loading;
