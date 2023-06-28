a1ffc30f960681ad18b9953eabe1542f121618d8
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-28 20:40:51 +0600

    fixed registration error

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -21,5 +21,5 @@ export default async function handler(
   };
 
   const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-  return response.send(authResponse);
+  return response.json(authResponse);
 }
